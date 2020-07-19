3af4bc6dabf7755b6fafec480a8c84fd03068c52
Dilan Isuru Jayasinghe <59989533+DilaN97514@users.noreply.github.com>
2020-07-20 01:06:35 +0530

    Some lables changed

---
+++ MediHelp-System/Form2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MediHelp_System
+{
+    public partial class pha_reg : Form
+    {
+        public pha_reg()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void reset_button_Click(object sender, EventArgs e)
+private void SignUP_button_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
